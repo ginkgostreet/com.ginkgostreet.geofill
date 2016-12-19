7859f465037c6693af5503bafda2be1ca4c0abd6
Frank J. Gómez <frank@ginkgostreet.com>
2016-12-18 19:20:49 -0500

    Added settings. Fleshed out util method for determining which fields we should write to based on the configured policy.

---
+++ CRM/Geofill/Utils.php
@@ -0,0 +1,41 @@
+<?php
+
+class CRM_Geofill_Utils {
+
+  const POLICY_IGNORE = 0;
+  const POLICY_FILL = 1;
+  const POLICY_OVERWRITE = 2;
+
+  /**
+   * Returns a list of address fields that should be updated based on the
+   * configured policy.
+   *
+   * @param array $addressComponents
+   *   The address that was passed to the geocoder.
+   * @return array
+   *   The fields for which updates are permitted.
+   */
+  public static function getWritableAddressComponents($addressComponents) {
+    $policy = Civi::settings()->get('geofill_field_policy');
+
+    $writable = array();
+    foreach ($policy as $fieldName => $flag) {
+      switch ($flag) {
+        case self::POLICY_OVERWRITE:
+          $writable[] = $fieldName;
+          break;
+        case self::POLICY_FILL:
+          // For some reason, some empty address components are represented with
+          // a string 'null.' Arguably, this is a core bug that should be
+          // corrected rather than worked around.
+          if (!CRM_Utils_Array::value($fieldName, $addressComponents) || CRM_Utils_Array::value($fieldName, $addressComponents) === 'null') {
+            $writable[] = $fieldName;
+          }
+          break;
+      }
+    }
+
+    return $writable;
+  }
+
+}
---
+++ settings/policy.setting.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+  'geofill_field_policy' => array(
+    'group_name' => 'Geodata Filler',
+    'group' => 'com.ginkgostreet.geofill',
+    'name' => 'geofill_field_policy',
+    'type' => 'Array',
+    'default' => array(
+      'city' => CRM_Geofill_Utils::POLICY_FILL,
+      'county_id' => CRM_Geofill_Utils::POLICY_FILL,
+      'country_id' => CRM_Geofill_Utils::POLICY_FILL,
+      'postal_code' => CRM_Geofill_Utils::POLICY_FILL,
+      'postal_code_suffix' => CRM_Geofill_Utils::POLICY_FILL,
+      'state_province_id' => CRM_Geofill_Utils::POLICY_FILL,
+    ),
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'help_text' => ts("Set a policy for each address field for handling additional data returned from a geocoding service. Data can be discarded (CiviCRM's default behavior), used to fill in missing data, or used to overwrite existing data.", array('domain' => 'com.ginkgostreet.geofill')),
+  ),
+);
